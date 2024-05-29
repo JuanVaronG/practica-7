6d4f79c500eb4af6aaf7ac964503651a3cf39476
JuanVaronG <juanchoavid_57@hotmail.com>
2024-05-28 20:13:09 -0500

    agregar mongo db

---
+++ db.js
@@ -0,0 +1,10 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://juanchoavid57:<Yujuan1112*>@curso-full-stack.r33wq9a.mongodb.net/?retryWrites=true&w=majority&appName=curso-full-stack";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
